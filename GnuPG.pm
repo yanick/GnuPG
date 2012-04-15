d4cad72e5552c4d881e0090074a7a4d5e1f2f4c6
Yanick Champoux <yanick@babyl.dyndns.org>
2012-04-15 11:43:08 -0400

    add comment wrt version message was introduced

--- GnuPG.pm
+++ GnuPG.pm
@@ -754,6 +754,7 @@ sub decrypt_postwrite($%) {
     } else {
         # gnupg 1.0.2 adds this status message
         ( $cmd, $arg ) = $self->read_from_status() if $cmd =~ /BEGIN_DECRYPTION/;
+        # gnupg 1.4.12 adds this status message
         ( $cmd, $arg ) = $self->read_from_status() if $cmd =~ /DECRYPTION_INFO/;
 
         $self->abort_gnupg( "invalid passphrase - $cmd" ) unless $cmd =~ /PLAINTEXT/;
